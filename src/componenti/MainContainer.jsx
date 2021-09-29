3739bdeea365cacdc80ccc937a57722b6e23ced7
unknown <federico.conti@magneticdays.com>
2021-09-29 12:28:23 +0200

    aggiunto router e container framework

--- src/componenti/MainContainer.jsx
+++ src/componenti/MainContainer.jsx
@@ -1,9 +1,36 @@
-import React from "react";
+import React from "react"
+import { useState } from "react"
+import Button from '@mui/material/Button'
+import styles from './MainContainer.module.css'
+import { useTranslation } from 'react-i18next'
+import ContainerFramework from "./scriviFramewrok/ContainerFramework"
 
 const MainContainer = () => {
+
+    const [pagina, setPagina] = useState("menu_princ")
+
+    const { t, i18n } = useTranslation()
+
     return (
         <div>
-            
+            {pagina==="menu_princ" ?
+                <div className={styles.container}>
+                    <div className={styles.containerBottoni}>
+                        <Button variant="contained" className={styles.bottone} onClick={() => setPagina("scrivi_frame")}>
+                            {t('main-container:scrivi-framework')}</Button>
+
+                        <Button variant="contained" className={styles.bottone} onClick={() => setPagina("modifica_frame")}>
+                            {t('main-container:modifica-framework')}</Button>
+
+                        <Button variant="contained" className={styles.bottone} onClick={() => setPagina("esporta")}>
+                            {t('main-container:esporta')}</Button>
+                    </div>
+                </div> :
+            pagina==="scrivi_frame" ?
+                <ContainerFramework /> :
+            pagina==="modifica_frame" ?
+            console.log(2) :
+            console.log(3)}
         </div>
     )
 }
