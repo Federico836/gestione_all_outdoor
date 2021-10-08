2a4bb254e3981eb9179e06af7b8df64263e90ef8
unknown <federico.conti@magneticdays.com>
2021-10-08 12:08:19 +0200

    aggiunti tempo e distanza totale

--- src/componenti/scriviFramewrok/corsa/tabelle/TabCorsaDragNDrop.jsx
+++ src/componenti/scriviFramewrok/corsa/tabelle/TabCorsaDragNDrop.jsx
@@ -4,6 +4,7 @@ import styles from './TabCorsaDragNDrop.module.css'
 import { useTranslation } from 'react-i18next'
 import { scambioElementiArray } from '../../../../utils/funzioniArray'
 import { getSecondsFromHHMMSS, toHHMMSS } from '../../../../utils/funzioni'
+import { calcolaDistanzaTot, calcolaTempo } from './funzioniCorsa.js'
 
 const Row = (props) => {
 
@@ -11,43 +12,17 @@ const Row = (props) => {
     
     const { t, i18n } = useTranslation()
 
-    const calcolaDistanzaTot = () => {
-        let distanzaTot = riga.distanza
-        if(isFinite(riga.serie) && riga.serie!=="" && riga.serie!==0) {
-            distanzaTot *= riga.serie
-        }
-        if(isFinite(riga.ripetizioni) && riga.ripetizioni!=="" && riga.ripetizioni!==0) {
-            distanzaTot *= riga.ripetizioni
-        }
-        return distanzaTot/1000
-    }
-
-    const calcolaTempo = () => {
-        let tempo = riga.passoMedia*riga.distanza/1000
-        console.log(riga.distanza)
-        let recupero = getSecondsFromHHMMSS(riga.recupero)
-        if(isFinite(riga.serie) && riga.serie!=="" && riga.serie!==0) {
-            tempo *= riga.serie
-            recupero *= riga.serie
-        }
-        if(isFinite(riga.ripetizioni) && riga.ripetizioni!=="" && riga.ripetizioni!==0) {
-            tempo *= riga.ripetizioni
-            recupero *= riga.ripetizioni
-        }
-        return toHHMMSS(tempo+recupero)
-    }
-
     return (
         <div className={styles.containerTab}>    
             <div style={{border: '1px solid gray', width: '10%', textAlign: 'center', display: "flex", alignItems: "center"}}><span>{riga.zona.descrizione}</span></div>
             <div style={{border: '1px solid gray', width: '10%', textAlign: 'center', display: "flex", alignItems: "center"}}><span>{riga.serie}</span></div>
             <div style={{border: '1px solid gray', width: '10%', textAlign: 'center', display: "flex", alignItems: "center"}}><span>{riga.ripetizioni}</span></div>
-            <div style={{border: '1px solid gray', width: '10%', textAlign: 'center', display: "flex", alignItems: "center"}}><span>{riga.distanza/1000}</span></div>
+            <div style={{border: '1px solid gray', width: '10%', textAlign: 'center', display: "flex", alignItems: "center"}}><span>{(riga.distanza/1000).toFixed(3)}</span></div>
             <div style={{border: '1px solid gray', width: '10%', textAlign: 'center', display: "flex", alignItems: "center"}}><span>{riga.recupero}</span></div>
-            <div style={{border: '1px solid gray', width: '10%', textAlign: 'center', display: "flex", alignItems: "center"}}><span>{calcolaTempo()}</span></div>
+            <div style={{border: '1px solid gray', width: '10%', textAlign: 'center', display: "flex", alignItems: "center"}}><span>{calcolaTempo(riga)}</span></div>
             <div style={{border: '1px solid gray', width: '10%', textAlign: 'center', display: "flex", alignItems: "center"}}>
                 <span>{riga.zona.zona>1 && riga.zona.zona<6 ? toHHMMSS(riga.passoMin)+"-"+toHHMMSS(riga.passoMax) : toHHMMSS(riga.passoMax)}</span></div>
-            <div style={{border: '1px solid gray', width: '10%', textAlign: 'center', display: "flex", alignItems: "center"}}><span>{calcolaDistanzaTot()}</span></div>
+            <div style={{border: '1px solid gray', width: '10%', textAlign: 'center', display: "flex", alignItems: "center"}}><span>{calcolaDistanzaTot(riga)}</span></div>
             <div style={{border: '1px solid gray', width: '30%', textAlign: 'center', display: "flex", alignItems: "center"}}><span>{riga.note}</span></div>
             <div style={{border: '1px solid gray', width: '8%', textAlign: 'center', cursor: "pointer", display: "flex", alignItems: "center"}}
                 onClick={() => aggiungiRiga(riga)}><span>📋</span></div>
@@ -85,11 +60,11 @@ const Lista = (props) => {
         setListaRighe(scambioElementiArray(listaRighe, oldIndex, newIndex))
     }
 
+    let totDistanza = 0
     let totTempo = 0
-    let totRecupero = 0
     for(let c=0;c<listaRighe.length;c++) {
-        totTempo += getSecondsFromHHMMSS(listaRighe[c].tempo)
-        totRecupero += getSecondsFromHHMMSS(listaRighe[c].recupero)
+        totDistanza += calcolaDistanzaTot(listaRighe[c])
+        totTempo += getSecondsFromHHMMSS(calcolaTempo(listaRighe[c]))
     }
 
     return (
@@ -115,10 +90,10 @@ const Lista = (props) => {
             <div style={{border: '1px solid gray', width: '10%', textAlign: 'center'}}></div>
             <div style={{border: '1px solid gray', width: '10%', textAlign: 'center'}}></div>
             <div style={{border: '1px solid gray', width: '10%', textAlign: 'center'}}></div>
-            <div style={{border: '1px solid gray', width: '10%', textAlign: 'center'}}>Tot: {toHHMMSS(totRecupero)}</div>
+            <div style={{border: '1px solid gray', width: '10%', textAlign: 'center'}}></div>
             <div style={{border: '1px solid gray', width: '10%', textAlign: 'center'}}>Tot: {toHHMMSS(totTempo)}</div>
             <div style={{border: '1px solid gray', width: '10%', textAlign: 'center'}}></div>
-            <div style={{border: '1px solid gray', width: '10%', textAlign: 'center'}}></div>
+            <div style={{border: '1px solid gray', width: '10%', textAlign: 'center'}}>Tot: {totDistanza.toFixed(3)}</div>
             <div style={{border: '1px solid gray', width: '30%', textAlign: 'center'}}></div>
             <div style={{border: '1px solid gray', width: '8%', textAlign: 'center'}}></div>
             <div style={{border: '1px solid gray', width: '8%', textAlign: 'center'}}></div>
