3bb0f57e82e4d9d44e3a938366794c736e0f300b
unknown <federico.conti@magneticdays.com>
2021-10-14 16:07:54 +0200

    sistemata palestra

---
+++ src/componenti/scriviFramewrok/palestra/tabelle/TabPalestraAddRiga.jsx
@@ -0,0 +1,60 @@
+import React from "react"
+import Button from '@mui/material/Button'
+import { useTranslation } from 'react-i18next'
+import { getSecondsFromHHMMSS, toHHMMSS } from "../../../../utils/funzioni.js"
+import styles from './TabPalestraAddRiga.module.css'
+
+const TabPalestraAddRiga = (props) => {
+
+    const { aggiungiRiga, datiSingolaRiga, setDatiSingolaRiga, modificaRiga } = props
+
+    const { t, i18n } = useTranslation()
+
+    /* const onBlurDurata = (event) => {
+        const value = event.target.value;
+        const seconds = Math.max(0, getSecondsFromHHMMSS(value))
+        const time = toHHMMSS(seconds)
+        setDatiSingolaRiga({...datiSingolaRiga, durata: time})
+    } */
+
+    const onBlurRecupero = (event) => {
+        const value = event.target.value;
+        const seconds = Math.max(0, getSecondsFromHHMMSS(value))
+        const time = toHHMMSS(seconds)
+        setDatiSingolaRiga({...datiSingolaRiga, recupero: time})
+    }
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.containerTab}>
+                <table className={styles.tabella}>
+                    <thead>
+                        <th style={{textAlign: 'center'}}>{t('scrivi-framework:palestra:esercizio')}</th>
+                        <th style={{textAlign: 'center'}}>{t('scrivi-framework:palestra:serie')}</th>
+                        <th style={{textAlign: 'center'}}>{t('scrivi-framework:palestra:ripetizioni')}</th>
+                        <th style={{textAlign: 'center'}}>{t('scrivi-framework:palestra:peso')}</th>
+                        <th style={{textAlign: 'center'}}>{t('scrivi-framework:palestra:recupero')}</th>
+                        <th style={{textAlign: 'center'}}>TUT</th>
+                        <th style={{textAlign: 'center'}}>Note</th>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><input type="text" onChange={e => {setDatiSingolaRiga({...datiSingolaRiga, esercizio: e.target.value})}} value={datiSingolaRiga.esercizio} /></td>
+                            <td><input type="number" min="0" value={datiSingolaRiga.serie} onChange={e => {setDatiSingolaRiga({...datiSingolaRiga, serie: e.target.value})}} /></td>
+                            <td><input type="number" min="0" value={datiSingolaRiga.ripetizioni} onChange={e => {setDatiSingolaRiga({...datiSingolaRiga, ripetizioni: e.target.value})}} /></td>
+                            <td><input type="number" min="0" value={datiSingolaRiga.peso} onChange={e => {setDatiSingolaRiga({...datiSingolaRiga, peso: e.target.value})}} /></td>
+                            <td><input type="text" onChange={e => {setDatiSingolaRiga({...datiSingolaRiga, recupero: e.target.value})}} onBlur={onBlurRecupero} value={datiSingolaRiga.recupero} /></td>
+                            <td><input type="text" onChange={e => {setDatiSingolaRiga({...datiSingolaRiga, tut: e.target.value})}} value={datiSingolaRiga.tut} /></td>
+                            <td><input type="text" value={datiSingolaRiga.note} onChange={e => {setDatiSingolaRiga({...datiSingolaRiga, note: e.target.value})}} /></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div className={styles.bottoneAdd}>
+                <Button variant="contained" onClick={() => aggiungiRiga(datiSingolaRiga)}>{modificaRiga ? "✎" : "➕"}</Button>
+            </div>
+        </div>
+    )
+}
+
+export default TabPalestraAddRiga
