5db69962b082141301f6bbe6c569725b42386e69
unknown <federico.conti@magneticdays.com>
2022-06-30 17:30:34 +0200

    cambiata velocita grafico lattato

--- src/componenti/containerAnalisiTest/corsa/testMader/containerTabelle/graficoLattato/GraficoLattato.jsx
+++ src/componenti/containerAnalisiTest/corsa/testMader/containerTabelle/graficoLattato/GraficoLattato.jsx
@@ -8,10 +8,12 @@ const GraficoLattato = props => {
 
     const { t, i18n } = useTranslation()
 
+    const datiGrafico = puntiSelected.map(riga => ({...riga, velKmh: Math.round(riga.velKmh*10)/10}))
+
     return (
         <div className={styles.container}>
             <ResponsiveContainer width="100%" height={400}>
-                <LineChart data={puntiSelected} margin={{ top: 10, right: 10, bottom: 10, left: 10 }}>
+                <LineChart data={datiGrafico} margin={{ top: 10, right: 10, bottom: 10, left: 10 }}>
                 <Line type="monotone" yAxisId="left" dataKey="lattato" stroke="blue" fill="blue" r={4} isAnimationActive={false} dot={true} />
                 <Line type="monotone" yAxisId="right" dataKey="heartrate" stroke="red" fill="red" r={4} isAnimationActive={false} dot={true} />
             
@@ -21,7 +23,7 @@ const GraficoLattato = props => {
                 <ReferenceLine yAxisId="left" stroke="red" strokeDasharray="3 3" segment={[{ x: tabTotali.velKmh2, y: 0 }, { x: tabTotali.velKmh2, y: lattatoTabTotali.lattato2 }]} />
 
                 <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-                <XAxis dataKey={"velKmh"} type="number" /* domain={['dataMin-10', 'dataMax+10']} */ tickCount={14} >
+                <XAxis dataKey={"velKmh"} type="number" domain={['dataMin-1', 'dataMax+1']} tickCount={14} >
                     <Label value={t('analisi-test:corsa:mader:velocita')} offset={-8} position="insideBottomRight" fill='#676767' fontSize={14} />
                 </XAxis>
 
